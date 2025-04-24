7c4280fa10c6035efddeedfcf40466d6d3b15cc6
kaicherry <kaicherry@mac.com>
2025-04-23 17:58:37 -0700

    Update install.js
    Pulling from different Mac repo

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/brandon929/FramePack.git app",
+          "git clone -b AvgPool3dToConv3d https://github.com/mdaiter/FramePack.git app",
         ]
       }
     },
